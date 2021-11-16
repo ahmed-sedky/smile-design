f368e26aa8565c05143c9807228ebfbe3d146c0b
Ahmed Abdelfattah <ama.elfattah@gmail.com>
2021-11-16 02:34:20 +0200

    Add discoloration option

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from PyQt5 import QtWidgets, uic, QtGui
-from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMainWindow
 import sys
-import helper as helper
+import helper as Helper
+import face as Face
 
 
 class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi("app.ui", self)
-        self.openImage.triggered.connect(lambda: helper.browsefiles(self))
+        self.openImage.triggered.connect(lambda: Helper.browsefiles(self))
+        self.discoloration.triggered.connect(lambda: Face.checkDiscoloration(self))
 
 
 if __name__ == "__main__":
--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
-import image as Image
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtWidgets, QtCore
 from pathlib import Path
+import face as Face
 
 
 def browsefiles(self):
@@ -11,8 +13,10 @@ def browsefiles(self):
     fileName = Path(filePath).stem
     extensionsToCheck = (".jpg", ".png", ".jpeg")
     if fname[0].endswith(extensionsToCheck):
-        Image.plotImage(self, filePath)
+        plotImage(self, filePath)
         setLabel(self, fileName)
+        Face.mouthDetection(filePath)
+        enableActions(self)
     elif fname[0] != "":
         errorMssg(self, "Invalid format.")
         return
@@ -20,9 +24,28 @@ def browsefiles(self):
         return
 
 
+def plotImage(self, imagePath):
+    pixelMap = QPixmap(imagePath).scaled(
+        560, 560, QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation
+    )
+    item = QtWidgets.QGraphicsPixmapItem(pixelMap)
+    scene = QtWidgets.QGraphicsScene()
+    scene.addItem(item)
+    self.image.setScene(scene)
+
+
 def setLabel(self, label):
     self.imageName.setText(label)
 
 
+def enableActions(self):
+    self.discoloration.setEnabled(True)
+    self.midline.setEnabled(True)
+
+
 def errorMssg(self, txt):
     QMessageBox.critical(self, "Error", txt)
+
+
+def popMssg(self, txt):
+    QMessageBox.information(self, "Result", txt)
